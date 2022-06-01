15515e7b05091d47db7385cc75b408b4d1a9f1f2
GedeD <dyavas21@gmail.com>
2022-06-01 21:20:23 +0700

    signin, signup

--- splitbill/lib/main.dart
+++ splitbill/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:splitbill/page/onboarding_page.dart';
+import 'package:splitbill/page/register_page.dart';
+import 'package:splitbill/page/sign_in_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +15,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routes: {
         '/': (context) => OnboardingPage(),
+        '/sign-in': (context) => SignInPage(),
+        '/register': (context) => RegisterPage(),
       },
     );
   }
---
+++ splitbill/lib/page/sign_in_page.dart
@@ -0,0 +1,342 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import '../theme.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  late FToast fToast;
+  final emailController = TextEditingController(text: '');
+  final passwordController = TextEditingController(text: '');
+
+  bool isShowPasswordError = false;
+  bool isRememberMe = false;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fToast = FToast();
+    fToast.init(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          tittle(),
+          emailInput(),
+          passwordInput(),
+          rememberCheckbox(),
+          loginButton(),
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(
+                top: 24,
+              ),
+              child: Text(
+                'OR',
+                style: greyTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: semibold,
+                ),
+              ),
+            ),
+          ),
+          loginWithGoogleButton(),
+          registerButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget tittle() {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 84,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Login to your\naccount',
+            style: blackAccentTextStyle.copyWith(
+              fontSize: 24,
+              fontWeight: bold,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Container(
+                width: 87,
+                height: 4,
+                margin: EdgeInsets.only(
+                  right: 4,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Color(0xffED193F),
+                ),
+              ),
+              Container(
+                width: 8,
+                height: 4,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Color(0xffED193F),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget emailInput() {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 48,
+      ),
+      padding: EdgeInsets.all(
+        16,
+      ),
+      decoration: BoxDecoration(
+        color: kWhiteGreyColor,
+        borderRadius: BorderRadius.circular(
+          14,
+        ),
+      ),
+      child: TextFormField(
+        controller: emailController,
+        decoration: InputDecoration.collapsed(
+          hintText: 'Email',
+          hintStyle: greyTextStyle.copyWith(
+            fontSize: 16,
+            fontWeight: semibold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget passwordInput() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: EdgeInsets.only(
+            top: 32,
+          ),
+          padding: EdgeInsets.all(
+            16,
+          ),
+          decoration: BoxDecoration(
+            color: kWhiteGreyColor,
+            borderRadius: BorderRadius.circular(
+              14,
+            ),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'Password',
+                    hintStyle: greyTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: semibold,
+                    ),
+                  ),
+                ),
+              ),
+              Icon(
+                Icons.visibility_outlined,
+                color: kGreyColor,
+              ),
+            ],
+          ),
+        ),
+        if (isShowPasswordError)
+          Container(
+            margin: EdgeInsets.only(top: 8),
+            child: Text(
+              'Password kamu salah',
+              style: redTextStyle,
+            ),
+          ),
+      ],
+    );
+  }
+
+  Widget rememberCheckbox() {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 32,
+      ),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 20,
+            height: 20,
+            child: Checkbox(
+              value: isRememberMe,
+              onChanged: (value) {
+                setState(() {
+                  isRememberMe = value!;
+                });
+              },
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(4),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Text(
+            'Remember me',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget loginButton() {
+    return Container(
+      height: 56,
+      width: double.infinity,
+      margin: EdgeInsets.only(
+        top: 32,
+      ),
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            isLoading = true;
+          });
+
+          Future.delayed(Duration(seconds: 2), () {
+            setState(() {
+              isLoading = false;
+            });
+            if (passwordController.text != '123') {
+              setState(() {
+                isShowPasswordError = true;
+              });
+              fToast.showToast(
+                child: errorToast(),
+                toastDuration: Duration(seconds: 2),
+                gravity: ToastGravity.BOTTOM,
+              );
+            } else {
+              Navigator.pushNamed(context, '/home');
+            }
+          });
+        },
+        style: TextButton.styleFrom(
+          backgroundColor: Color(0xffFFC107),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(14),
+          ),
+        ),
+        child: isLoading
+            ? CircularProgressIndicator(
+                color: kWhiteColor,
+                backgroundColor: kGreyColor,
+              )
+            : Text(
+                'Login',
+                style: whiteTextStyle.copyWith(
+                    fontSize: 18, fontWeight: semibold, color: kBlackColor),
+              ),
+      ),
+    );
+  }
+
+  Widget loginWithGoogleButton() {
+    return Container(
+      height: 56,
+      width: double.infinity,
+      margin: EdgeInsets.only(
+        bottom: 21,
+      ),
+      child: TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(14),
+          ),
+        ),
+        child: Text(
+          'Login with Google',
+          style: blackTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: semibold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget registerButton() {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 48,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Don\'t have an account?',
+            style: greyTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semibold,
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/register');
+            },
+            child: Text(
+              'Register',
+              style: blueTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: semibold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget errorToast() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: kRedColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        'Password salah',
+        style: whiteTextStyle.copyWith(
+          fontSize: 16,
+          fontWeight: semibold,
+        ),
+      ),
+    );
+  }
+}
