e5d4982f6450ef8eecfe2dafa03e64d41f3c63d4
GedeD <dyavas21@gmail.com>
2022-06-02 15:05:59 +0700

    dashboard1

--- splitbill/lib/page/dashboard_page.dart
+++ splitbill/lib/page/dashboard_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:splitbill/theme.dart';
 
 class DashboardPage extends StatelessWidget {
@@ -8,22 +9,71 @@ class DashboardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff343A40),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 24,
-            ),
-            child: Row(
-              children: [
-                SizedBox(
-                  height: 65,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: 65,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Hi Gede',
+                          style: GoogleFonts.nunito(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'SplitBill',
+                          style: GoogleFonts.nunito(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/profile1.png',
+                    width: 60,
+                    height: 60,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 23,
+              ),
+              Container(
+                height: 275,
+                width: MediaQuery.of(context).size.width - 48,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                Text('Hi Gede')
-              ],
-            ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [],
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
